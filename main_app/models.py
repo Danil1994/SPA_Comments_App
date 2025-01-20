22b20f93f41850f759f3b1d3bd51096f422a7ab6
Danil1994 <danilaleksandrovich36@gmail.com>
2025-01-20 10:35:48 +0200

    Feat: create XSS defence

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,18 @@
+import bleach
+
 from django.db import models
+
+
+ALLOWED_TAGS = ['a', 'code', 'i', 'strong']
+ALLOWED_ATTRIBUTES = {
+    'a': ['href', 'title']
+}
 
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=50, verbose_name="User Name")
     email = models.EmailField(verbose_name="Email")
     home_page = models.URLField(blank=True, null=True, verbose_name="Home Page")
-    # captcha = models.CharField(max_length=10, verbose_name="CAPTCHA")
     text = models.TextField(verbose_name="Text")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     image = models.ImageField(
@@ -31,6 +38,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user_name} - {self.text[:30]}"
+
+    def clean(self):
+        super().clean()
+        self.text = bleach.clean(
+            self.text,
+            tags=ALLOWED_TAGS,
+            attributes=ALLOWED_ATTRIBUTES,
+            strip=True
+        )
 
     class Meta:
         ordering = ['-created_at']

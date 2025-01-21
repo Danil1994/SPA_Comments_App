560cae448a7aa74399a55177ef62b6e209eb4ba6
Danil1994 <danilaleksandrovich36@gmail.com>
2025-01-21 10:16:28 +0200

    Fix: basic react works

--- react_comment/src/services/api.js
+++ react_comment/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/comments";
+const API_URL = "http://127.0.0.1:8000/api/comments/";
 
 // Get comments
 export const fetchComments = (page = 1, sortBy = "created_at", order = "desc") =>
@@ -11,4 +11,4 @@ export const addComment = (data) => axios.post(API_URL, data);
 
 // Get answers to comment
 export const fetchReplies = (parentId, limit = 10) =>
-  axios.get(`${API_URL}/${parentId}/replies?limit=${limit}`);
+  axios.get(`${API_URL}${parentId}/replies/?limit=${limit}`);

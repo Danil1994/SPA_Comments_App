fe75149ade4ecfdf6c9ef8d05ac7a6b915b64118
Danil1994 <danilaleksandrovich36@gmail.com>
2025-01-22 15:45:58 +0200

    Feat: add HomePage.css

--- react_comment/src/pages/HomePage.jsx
+++ react_comment/src/pages/HomePage.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import CommentForm from "../components/CommentForm";
 import CommentList from "../components/CommentList";
+import "../css/HomePage.css"; // Подключаем стили
 
 const HomePage = () => {
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <div>
+    <div className="home-page">
       <h1>Комментарии</h1>
+
       {/* Кнопка для переключения отображения формы */}
       {!showForm && (
-        <button onClick={() => setShowForm(true)}>Написать комментарий</button>
+        <button className="form-toggle-button" onClick={() => setShowForm(true)}>
+          Написать комментарий
+        </button>
       )}
 
       {/* Отображение формы, если showForm === true */}
       {showForm && (
-        <div>
+        <div className="form-container">
           <CommentForm onAdd={() => window.location.reload()} />
-          <button onClick={() => setShowForm(false)}>Отмена</button>
+          <button className="cancel-button" onClick={() => setShowForm(false)}>
+            Отмена
+          </button>
         </div>
       )}
 

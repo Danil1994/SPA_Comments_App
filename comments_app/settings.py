39a694197426bf8d20f9176476f4ad7dbe6e9e72
Danil1994 <danilaleksandrovich36@gmail.com>
2025-01-23 12:00:52 +0200

    Refactor: add  .env

--- comments_app/settings.py
+++ comments_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*%mc@0=znu^2!q7s7t(#uqcn!ho!*&)=oz7x#qxe1$kflb_sz'
+SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
